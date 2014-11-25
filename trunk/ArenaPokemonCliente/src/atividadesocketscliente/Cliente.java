1d12380b9c542f9759f3b5f5f1c733e21aadd339
dsatierry@gmail.com <dsatierry@gmail.com@4d66e365-09fa-031d-f66a-ddd2246c89b8>
2014-11-25 18:40:41 +0000

    Cliente para testes

---
+++ trunk/ArenaPokemonCliente/src/atividadesocketscliente/Cliente.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadesocketscliente;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author POP
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.net.UnknownHostException
+     */
+    public static void main(String[] args)
+            throws UnknownHostException, IOException {
+        // TODO code application logic here
+        Socket cliente = new Socket("192.168.2.105", 12345);
+
+        Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
+
+        saida.println("1;thiago;123456");
+        
+        // Imprime a mensagem recebida do servidor...
+        Scanner s = null;
+        s = new Scanner(cliente.getInputStream());
+        String teste = s.nextLine();
+        System.out.println("Servidor: " + teste);
+        // -------------- testes acima...
+
+        while (teclado.hasNextLine()) {
+            String palavra = teclado.nextLine();
+            saida.println(palavra);
+            if ("9999".equals(palavra)) {
+                System.out.println("Desconectado do servidor.");
+
+                break;
+            }
+        }
+
+        saida.close();
+        teclado.close();
+        cliente.close();
+    }
+}
