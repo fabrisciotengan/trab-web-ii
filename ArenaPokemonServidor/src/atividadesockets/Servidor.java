82b64909196f0947ba8a693afdc695a3b2c008f5
dsatierry@gmail.com <dsatierry@gmail.com@4d66e365-09fa-031d-f66a-ddd2246c89b8>
2014-11-26 05:49:44 +0000

    No servidor foi criado a parte de envio da matriz para a aplicação de mapa, ta praticamente finalizado.

--- ArenaPokemonServidor/src/atividadesockets/Servidor.java
+++ ArenaPokemonServidor/src/atividadesockets/Servidor.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,15 +24,38 @@ public class Servidor {
      * @param args the command line arguments
      * @throws java.io.IOException
      */
-    
     //Matriz do nosso joguinho =]
     static int m[][] = new int[5][5];
-    
+
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
+
         ServerSocket servidor = new ServerSocket(12345);
         System.out.println("Porta 12345 aberta!");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                m[i][j] = 0;
+            }
+        }
+        m[3][2] = 3;
+        m[4][1] = 4;
+
+        //Variáveis para testar o envio dos dados para a aplicação de mapa.
+        LinkedList listaLigada = new LinkedList();
+        ClienteClasse clientes = new ClienteClasse();
+        ClienteClasse clientes1 = new ClienteClasse();
+        clientes.setId("3");
+        clientes.setPokemon("1");
+        clientes.setVida("100");
+        clientes.setDirecao("2");
+        listaLigada.add(clientes);
+
+        clientes1.setId("4");
+        clientes1.setPokemon("3");
+        clientes1.setVida("100");
+        clientes1.setDirecao("4");
+        listaLigada.add(clientes1);
 
         while (true) {
             Socket cliente = servidor.accept();
@@ -41,47 +65,74 @@ public class Servidor {
                 public void run() {
                     System.out.println(cliente.getInetAddress().getHostAddress() + " Conectou ao servidor");
                     Scanner s = null;
+                    PrintStream saida;
 
                     try {
                         s = new Scanner(cliente.getInputStream());
                     } catch (IOException ex) {
                         Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
+
                     //Aqui será a parte onde o servidor irá verificar se é o cara do mapa que está requisitando informações.
-                    /*if(s.hasNextLine() && "101".equals(s.nextLine())){
-                    }*/
-                    
-                    //Aqui é onde realiza a autenticação do usuário e inicia suas ações.
-                    if (s.hasNextLine() && "1;thiago;123456".equals(s.nextLine())) {
-                        
-                        System.out.println(cliente.getInetAddress().getHostAddress() + " Logou-se ao jogo");
-                        
-                        // Envia mensagem ao cliente, desejando boas vindas.   
-                        PrintStream saida;
-                        try {
-                            saida = new PrintStream(cliente.getOutputStream());
-                            saida.println("Seja bem vindo ao servidor.");
-                        } catch (IOException ex) {
-                            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
-                        }                       
-                        
-                        // Aqui o servidor fica em um loop infinito recebendo informações do cliente até que ele envie "9999", que é onde sai do loop.
-                        while (s.hasNextLine()) {
-                            String codigo = s.nextLine();
-                            System.out.println(codigo);
-                            if ("9999".equals(codigo)) {
-                                System.out.println(cliente.getInetAddress().getHostAddress() + " Desconectou-se");
-                                break;
+                    if (s.hasNextLine() && "101".equals(s.nextLine())) {
+                        String verJogadores = "101;";
+                        String posicao;
+
+                        //Aqui há um for para percorrer a lista pegando os usuários e formando a string a ser enviada à aplicação mapa.
+                        for (int i = 0; i < 5; i++) {
+                            for (int j = 0; j < 5; j++) {
+                                if (m[i][j] != 0 && m[i][j] != 1 && m[i][j] != 2) {
+                                    posicao = Integer.toString(m[i][j]);
+                                    for (int k = 0; k < listaLigada.size(); k++) {
+                                        ClienteClasse criente = (ClienteClasse) listaLigada.get(k);
+                                        if (criente.getId() == null ? posicao == null : criente.getId().equals(posicao)) {
+                                            verJogadores = verJogadores + criente.getId() + ";" + criente.getPokemon() + ";" + criente.getVida() + ";" + posicao + ";" + criente.getDirecao() + "#";
+                                        }
+                                    }
+                                }
                             }
                         }
-                    } else {
-                        System.out.println(cliente.getInetAddress().getHostAddress() + " Falha ao conecatar-se ao jogo, login ou senha incorreta.");
-                        s.close();
+                        
+                        //Aqui é onde envia a string com a posição, id, vida... para o mapa.
                         try {
-                            cliente.close();
+                            saida = new PrintStream(cliente.getOutputStream());
+                            saida.println(verJogadores);
                         } catch (IOException ex) {
                             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    } else {
+
+                        //Aqui é onde realiza a autenticação do usuário e inicia suas ações.
+                        if (s.hasNextLine() && "1;thiago;123456".equals(s.nextLine())) {
+
+                            System.out.println(cliente.getInetAddress().getHostAddress() + " Logou-se ao jogo");
+
+                            // Envia mensagem ao cliente, desejando boas vindas.   
+                            try {
+                                saida = new PrintStream(cliente.getOutputStream());
+                                saida.println("Seja bem vindo ao servidor.");
+                            } catch (IOException ex) {
+                                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+
+                            // Aqui o servidor fica em um loop infinito recebendo informações do cliente até que ele envie "9999", que é onde sai do loop.
+                            while (s.hasNextLine()) {
+                                String codigo = s.nextLine();
+                                System.out.println(codigo);
+                                if ("9999".equals(codigo)) {
+                                    System.out.println(cliente.getInetAddress().getHostAddress() + " Desconectou-se");
+                                    break;
+                                }
+                            }
+                        } else {
+                            System.out.println(cliente.getInetAddress().getHostAddress() + " Falha ao conecatar-se ao jogo, login ou senha incorreta.");
+                            s.close();
+                            try {
+                                cliente.close();
+                            } catch (IOException ex) {
+                                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                            }
                         }
                     }
 
