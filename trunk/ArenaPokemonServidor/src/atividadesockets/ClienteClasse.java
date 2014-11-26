0d1ffd1c7a45073994a9b957f8997996dac61396
dsatierry@gmail.com <dsatierry@gmail.com@4d66e365-09fa-031d-f66a-ddd2246c89b8>
2014-11-26 05:49:44 +0000

    No servidor foi criado a parte de envio da matriz para a aplicação de mapa, ta praticamente finalizado.

---
+++ trunk/ArenaPokemonServidor/src/atividadesockets/ClienteClasse.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package atividadesockets;
+
+/**
+ *
+ * @author tierry
+ */
+public class ClienteClasse {
+    private String id;
+    private String login;
+    private String senha;
+    private String pokemon;
+    private String vida;
+    private String direcao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getPokemon() {
+        return pokemon;
+    }
+
+    public void setPokemon(String pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public String getVida() {
+        return vida;
+    }
+
+    public void setVida(String vida) {
+        this.vida = vida;
+    }
+
+    public String getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(String direcao) {
+        this.direcao = direcao;
+    }
+}
+
