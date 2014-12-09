418625edef3096fb8094a00937bcade800853221
dsatierry@gmail.com <dsatierry@gmail.com@4d66e365-09fa-031d-f66a-ddd2246c89b8>
2014-12-09 21:46:53 +0000

    Quase lá negada!

---
+++ TrabalhoSockets/src/trabalhosockets/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhosockets;
+
+/**
+ *
+ * @author Michel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LoginFrame login = new LoginFrame();
+        login.setVisible(true);
+        
+                
+//                if("100".equals(linha)){//comando para ver o mapa
+//                    saida.println(linha);
+//                    linha = entrada.readLine();
+//                    
+//                    if (linha == null) {
+//                        System.out.println("Conexão encerrada!");
+//                        break;
+//                    }
+//                    
+//                    System.out.println(linha);
+//                }
+                
+//                if("101".equals(linha)){//comando para ver os jogadores
+//                    saida.println(linha);
+//                    linha = entrada.readLine();
+//                    
+//                    if (linha == null) {
+//                        System.out.println("Conexão encerrada!");
+//                        break;
+//                    }
+//                    
+//                    System.out.println(linha);
+//                }
+//        }
+    }
+}
